c892b694b116811c9e5f816644cc365951a36b2b
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-27 14:15:58 -0500

    Create distinct.sql

---
+++ distinct.sql
@@ -0,0 +1,12 @@
+--• Eliminates duplicate values
+--• Used with COUNT function
+--• COUNT(DISTINCT expr) returns the number of distinct non-null values of expr.
+--• To display the number of distinct department values in the EMPLOYEES table:
+SELECT COUNT(DISTINCT department_id)
+FROM employees;
+--
+--
+--
+--
+--
+--
